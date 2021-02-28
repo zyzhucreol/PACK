2022b40d3d5b19e0955c9ff9f8bb513270c7cafa
Zheyuan Zhu <zyzhu@knights.ucf.edu>
2021-02-28 16:52:02 -0500

    MTF calculation working with cv2

--- vimba_cv2_test.py
+++ vimba_cv2_test.py
@@ -13,62 +13,7 @@ from vimba.frame import FrameStatus
 import cv2
 from scipy import fft
 
-#%% copied from video_test_santized example
-# from pyqtgraph.Qt import QtGui, QtCore
-# import pyqtgraph as pg
-# import pyqtgraph.ptime as ptime
-# import VideoTemplate
-
-# app = QtGui.QApplication([])
-
-# win = QtGui.QMainWindow()
-# win.setWindowTitle('Camera preview')
-# ui = VideoTemplate.Ui_MainWindow()
-# ui.setupUi(win)
-# win.show()
-
-# ui.maxSpin1.setOpts(value=255, step=1)
-# ui.minSpin1.setOpts(value=0, step=1)
-
-# vb = pg.ViewBox()
-# ui.graphicsView.setCentralItem(vb)
-# vb.setAspectLocked()
-# img = pg.ImageItem()
-# img.setCacheMode(1) #CacheMode of Qt, see https://doc.qt.io/qt-5/qgraphicsview.html#CacheModeFlag-enum
-# # img.setRect(QtCore.QRectF(0, 0, 512, 512))
-# vb.addItem(img)
-# vb.setRange(QtCore.QRectF(0, 0, 512, 512))
-
-
-# width = 1024
-# height = 1024
-
-# lastTime = ptime.time()
-# fps = None
-# def update(data_from_camera):
-#     global ui, lastTime, fps, img
-#     data_raw=np.abs(np.fft.fft2(data_from_camera))
-#     data_raw=data_raw/np.max(data_raw)*255
-#     data_on_the_fly=np.uint8(data_raw) # data must be uint8 if autoLevels=False
-#     img.setImage(data_on_the_fly, autoLevels=False)
-#     ui.stack.setCurrentIndex(0)
-
-#     now = ptime.time()
-#     dt = now - lastTime
-#     lastTime = now
-#     if fps is None:
-#         fps = 1.0/dt
-#     else:
-#         s = np.clip(dt*3., 0, 1)
-#         fps = fps * (1-s) + (1.0/dt) * s
-#     ui.fpsLabel.setText('%0.2f fps' % fps)
-#     app.processEvents()  ## force complete redraw for every plot
-# # timer = QtCore.QTimer()
-# # timer.timeout.connect(update)
-# # timer.start(0)
-
 #%% copied from Vimba example
-do_nothing=lambda *args: None
 crop_size=1024
 frame_row=2056
 frame_col=2464
@@ -85,10 +30,10 @@ X_flat=np.reshape(X,(-1),order='F')
 Y_flat=np.reshape(Y,(-1),order='F')
 n_radius=int(crop_size/2)
 R=np.sqrt((X-x_c)**2+(Y-y_c)**2)
-fft_data=np.zeros((1024,1024))
+fft_data=np.zeros((crop_size,crop_size))
 f_avg=lambda r_temp:fft_data[(R>=r_temp-0.5) & (R<r_temp+0.5)].mean()
 r=np.arange(1,n_radius)
-background=np.zeros((1024,1024),'int8')
+background=np.zeros((crop_size,crop_size),'int8')
 
 class Handler:
     def __init__(self):
@@ -115,10 +60,12 @@ class Handler:
             fft_data=np.log(np.abs(fft.fftshift(fft.fft2(data))))
             fft_data=fft_data/np.max(fft_data)
             fft_data_radial=np.vectorize(f_avg)(r)
-            cv2.imshow('test-cam',fft_data)
-            cv2.resizeWindow('test-cam',1024,768)
-            # cv2.polylines(background,[np.column_stack((r,fft_data_radial))])
-            # cv2.resizeWindow('MTF',1024,768)
+            cv2.imshow('Raw camera',data/np.max(data))
+            cv2.resizeWindow('Raw camera',1024,768)
+            fft_data_radial_uint8=255-np.uint8(fft_data_radial*255)
+            plot_img=cv2.polylines(background,[np.column_stack((r,fft_data_radial_uint8))],False,(255,255,255))
+            cv2.imshow('MTF',plot_img)
+            cv2.resizeWindow('MTF',512,256)
         cam.queue_frame(frame)
 
 def frame_handler(cam, frame):
